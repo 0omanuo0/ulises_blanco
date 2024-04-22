2c8f854a77ab1b987d21de628383c5116b0a9acc
0omanuo0 <manu365manu@gmail.com>
2024-04-22 22:04:52 +0000

    add nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
 import animations from "@midudev/tailwind-animations";
 
 const config: Config = {
@@ -6,6 +7,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -16,6 +18,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [animations],
+  darkMode: "class",
+  plugins: [animations, nextui()],
 };
 export default config;
