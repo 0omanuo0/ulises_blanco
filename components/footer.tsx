106a76913534ccd7eb885d9b521e0f6868412bd4
0omanuo0 <manu365manu@gmail.com>
2024-04-28 16:19:00 +0000

    add footer

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Footer() {
+    return (
+        <footer className="bg-gray-200 text-center border-t-2 border-black px-8 md:px-0 md:mx-24 space-y-2 py-4 mt-8 justify-between md:flex">
+            <p>© 2024 Ulises Blanco</p>
+            <a href="https://ulisesblanco.com">ulisesblanco.com</a>
+        </footer>
+    )
+}
