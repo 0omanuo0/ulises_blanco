5010f43857c495bf367be733aee639e23c25cdfa
0omanuo0 <manu365manu@gmail.com>
2024-07-30 11:26:36 +0000

    feat: Add legal terms and contact information to the page

---
+++ app/derechos/page.tsx
@@ -0,0 +1,93 @@
+
+
+
+export default function Derechos() {
+    return (
+        <main className="terminos-main">
+            <h1>Licencia de Uso de las Obras en ulisesblanco.com</h1>
+            <article className="terminos-legales">
+                <h2>1. Titularidad de Derechos</h2>
+                <p>
+                    Las obras presentadas en este sitio web (ulisesblanco.com) pertenecen exclusivamente a los hijos de Ulises Blanco. Todos los derechos están reservados.
+                </p>
+                <h2>2. Permiso de Uso</h2>
+                <p>Las obras pueden ser utilizadas libremente bajo las siguientes condiciones:</p>
+                <ul>
+                    <li>
+                        <h3>2.1 Atribución</h3>
+                        <p>
+                            Debe indicar claramente la procedencia de las obras, mencionando ulisesblanco.com como fuente original de manera visible en cualquier uso de las mismas.
+                        </p>
+                    </li>
+                    <li>
+                        <h3>2.2 Integridad de la Obra</h3>
+                        <p>
+                            Debe respetar la integridad y la imagen de las obras, evitando cualquier uso que las desvirtúe, menosprecie, o altere su significado original.
+                        </p>
+                    </li>
+                </ul>
+                <h2>3. Usos Prohibidos</h2>
+                <p>Los siguientes usos están prohibidos sin el consentimiento expreso y por escrito de los titulares:</p>
+                <ul>
+                    <li>
+                        <h3>3.1 Alteración y Transformación</h3>
+                        <p>
+                            No se permite la alteración, transformación o generación de obras derivadas.
+                        </p>
+                    </li>
+                    <li>
+                        <h3>3.2 Uso Comercial</h3>
+                        <p>
+                            No se permite el uso de las obras con fines comerciales.
+                        </p>
+                    </li>
+                    <li>
+                        <h3>3.3 Daño a la Imagen</h3>
+                        <p>
+                            No se permite el uso de las obras de manera que pueda dañar la imagen, la reputación o el honor de Ulises Blanco y sus herederos.
+                        </p>
+                    </li>
+                </ul>
+                <h2>4. Procedimiento para Solicitar Permisos</h2>
+                <p>
+                    Para solicitar permisos que no estén cubiertos por esta licencia, debe contactar a los titulares a través de la información de contacto proporcionada en ulisesblanco.com.
+                </p>
+                <h2>5. Cumplimiento y Consecuencias</h2>
+                <p>
+                    El uso de las obras implica la aceptación de estos términos. El incumplimiento de cualquiera de las condiciones aquí establecidas dará lugar a acciones legales pertinentes para la protección de los derechos de autor.
+                </p>
+                <h2>
+                    6. Exención de Responsabilidad
+                </h2>
+                <p>
+                    Los titulares no se hacen responsables por el uso indebido de las obras por parte de terceros que no cumplan con los términos de esta licencia.
+                </p>
+                <h2>
+                    7. Modificaciones a la Licencia
+                </h2>
+                <p>
+                    Los titulares se reservan el derecho de modificar los términos de esta licencia en cualquier momento, con efecto inmediato tras su publicación en ulisesblanco.com. Es responsabilidad del usuario revisar periódicamente los términos para asegurarse del cumplimiento.
+                </p>
+            </article>
+
+            {/* <span className="h-bar"></span> */}
+            <article className="contact-info">
+                <h2>
+                    Información de Contacto
+                </h2>
+                <p>
+                    Para más información o consultas, por favor contacte a los titulares a través de <a target="_blank" href="https://ulisesblanco.com">ulisesblanco.com</a>.
+                </p>
+                <div id="mails">
+                    <p> Carmen Blanco: <a href="mailto:carmenblancomar@hotmail.com">carmenblancomar@hotmail.com</a></p>
+                    <p> Marta Blanco: <a href="mailto:MARTA_BLANCO">MARTA_BLANCO</a></p>
+                    
+                </div>
+                <p>
+                    Al utilizar cualquier obra de ulisesblanco.com, usted acepta y se compromete a cumplir con los términos de esta licencia de uso.
+                </p>
+            </article>
+            <p>Última actualización: 30 de julio de 2024</p>
+        </main>
+    )
+}
